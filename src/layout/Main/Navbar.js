64f8ca41cde3c9d4a0a619a05102ec08dd1471e2
farjana islam <nipa.farzana87@gmail.com>
2023-01-14 17:50:16 +0600

    added logo

--- src/layout/Main/Navbar.js
+++ src/layout/Main/Navbar.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import logo from '../../images/logo.png'
 
 function Navbar() {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg navbar-light navbar-container text-white">
         <div className="container-fluid">
-          <Link to='/' className="navbar-brand">Navbar</Link>
+          <Link to='/' className="navbar-brand">
+            <div className='brand-logo'><img src={logo} alt="" /></div>
+          </Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
